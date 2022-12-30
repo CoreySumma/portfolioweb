a42c6045d0fa3165c58bc45737cc5572aad396c7
CoreySumma <CoreySumma@gmail.com>
2022-12-30 13:04:06 -0600

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import CTA from './CTA';
+import HeaderSocials from './HeaderSocials';
+import './header.css';
+import { BsChevronRight } from 'react-icons/bs';
+
+const Header = () => {
+  return (
+    <header id="home">
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Corey Summa</h1>
+        <h5 className="text-light">Full-stack Developer</h5>
+        <CTA />
+        <a href="#contact" className="scroll__down">
+        <BsChevronRight />
+        <BsChevronRight />
+        <BsChevronRight />
+        <BsChevronRight />
+        <BsChevronRight />
+        </a>
+        <HeaderSocials />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaAward } from 'react-icons/fa';
+import { VscFolderLibrary } from 'react-icons/vsc';
+import ME from '../../assets/me.JPEG';
+import './intro.css';
+
+const Intro = () => {
+  return (
+    <section id="about">
+      <h5>A little bit</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1 year</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>10+ Completed Projects</small>
+            </article>
+          </div>
+          <p>I am a motivated and enthusiastic learner with a passion for full stack developmentment. I have found the process of learning and creating full stack applications to be enjoyable and rewarding, and I am constantly seeking new opportunities to improve my skills and knowledge. I value collaboration and have found that learning from others has been an invaluable part of my growth as a developer. In the future, I hope to continue to learn and gain experience through new opportunities and challenges.</p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Intro
