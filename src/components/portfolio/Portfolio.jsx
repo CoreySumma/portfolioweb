583d530ddd232d1db257c0332afe5e4c5510529b
CoreySumma <CoreySumma@gmail.com>
2022-12-30 21:50:31 -0600

    tweak projects section and CSS

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import IMG1 from '../../assets/bookstore.png';
-import IMG2 from '../../assets/todo.png';
-import IMG3 from '../../assets/shelter.png';
-import IMG4 from '../../assets/fake.png';
+import IMG1 from '../../assets/spaceman.png';
+import IMG2 from '../../assets/codemonk.png';
+import IMG3 from '../../assets/mistate.png';
+import IMG4 from '../../assets/spotlabs.png';
 import IMG5 from '../../assets/news.png';
 import IMG6 from '../../assets/math.png';
 
@@ -12,63 +12,63 @@ const Portfolio = () => {
   const soloProjects = [
     {
       id: 1,
-      title: 'Bookstore',
+      title: 'SpaceMan',
       img: IMG1,
       description:
-        'BookStore app that allows users to store books and update progress',
-      technologies: 'React | Redux | Ruby on Rails',
-      link: 'https://melodic-boba-111583.netlify.app/',
-      github: 'https://github.com/Meri-MG/bookstore---react',
+        'A word guessing game that generates a random word and allows the user 5 attempts at guessing correct letters within it.  Do you have what it takes to save the Spaceman?',
+      technologies: 'Vanilla Javascript',
+      link: 'https://coreysumma.github.io/Spaceman/',
+      github: 'https://github.com/CoreySumma/Spaceman',
     },
     {
       id: 2,
-      title: 'FakeStore',
+      title: 'SpotLabs',
       img: IMG4,
       description:
-        'FakeStore app that provides information about available products and their details',
-      technologies: 'React | Redux',
-      link: 'https://fakestore-metrics.netlify.app/',
-      github: 'https://github.com/Meri-MG/Fakestore---react',
+        'A collaborate playlist application that allows a user to add/edit/delete songs on playlists that they create with their friends and family by embedding spotify directly onto the app.',
+      technologies: 'HTML | CSS |Javascript | Node | Express | MongoDB | Mongoose | Google Oauth',
+      link: 'https://mongoose-playlist-926.herokuapp.com/',
+      github: 'https://github.com/CoreySumma/mongoose-playlist',
     },
     {
       id: 3,
-      title: 'To-Do-App',
+      title: '[codemonk]',
       img: IMG2,
-      description: 'User friendly app to plan your day and manage tasks',
-      technologies: 'JavaScript | Webpack',
-      link: 'https://meri-mg.github.io/To-Do-List/dist/',
-      github: 'https://github.com/Meri-MG/To-Do-List',
+      description: 'A full CRUD, Django driven collaboration for students to challenge and learn from one another by posting coding challenges, commenting on posts, and liking comments.',
+      technologies: ' | HTML | CSS | Python | Django | Javascript',
+      link: 'http://codemonk-wars.herokuapp.com/',
+      github: 'https://github.com/ultimoakim/-codemonk-',
     },
     {
       id: 4,
-      title: 'Shelter',
+      title: 'MiState',
       img: IMG3,
       description:
-        'Fully responsive interactive website built based on Figma design',
-      technologies: 'JavaScript | CSS',
-      link: 'https://meri-mg.github.io/shelter/pages/main/index.html',
-      github: 'https://github.com/Meri-MG/shelter',
+        'A MERN stack mindfullness app designed to track a users emotional state. As a user you are able to edit entries and clean the slate to start each week fresh! Are you ready for some self reflection?',
+      technologies: 'HTML | CSS | JavaScript | React | Node | Express | MongoDB | Bootstrap',
+      link: 'https://mistate.herokuapp.com/',
+      github: 'https://github.com/CoreySumma/mern-mistate',
     },
-    {
-      id: 5,
-      title: 'World News',
-      img: IMG5,
-      description:
-        'Fully responsive interactive website built based on Adobe XD design',
-      technologies: 'JavaScript | CSS',
-      link: 'https://meri-mg.github.io/Unilab-world-news/',
-      github: 'https://github.com/Meri-MG/Unilab-world-news',
-    },
-    {
-      id: 6,
-      title: 'Math Resource',
-      img: IMG6,
-      description:
-        'Real-world group project which is still in progress and will provide educational platform for future young developers',
-      technologies: 'JavaScript | Scss | Python',
-      link: 'https://lukinoo.github.io/math-resource/',
-      github: 'https://github.com/lukinoo/math-resource',
-    },
+    // {
+    //   id: 5,
+    //   title: 'World News',
+    //   img: IMG5,
+    //   description:
+    //     'Fully responsive interactive website built based on Adobe XD design',
+    //   technologies: 'JavaScript | CSS',
+    //   link: 'https://meri-mg.github.io/Unilab-world-news/',
+    //   github: 'https://github.com/Meri-MG/Unilab-world-news',
+    // },
+    // {
+    //   id: 6,
+    //   title: 'Math Resource',
+    //   img: IMG6,
+    //   description:
+    //     'Real-world group project which is still in progress and will provide educational platform for future young developers',
+    //   technologies: 'JavaScript | Scss | Python',
+    //   link: 'https://lukinoo.github.io/math-resource/',
+    //   github: 'https://github.com/lukinoo/math-resource',
+    // },
   ];
 
   return (
