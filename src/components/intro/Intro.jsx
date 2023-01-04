0dd44132d00813de48c1ef84809dbcd6a670f1f2
CoreySumma <CoreySumma@gmail.com>
2023-01-03 20:01:09 -0600

    profile pic

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import { FaAward } from 'react-icons/fa';
 import { VscFolderLibrary } from 'react-icons/vsc';
 import { RiUserStarFill } from 'react-icons/ri';
-import ME from '../../assets/me.JPEG';
+import ME from '../../assets/me3.png';
 import './intro.css';
 
 const Intro = () => {
