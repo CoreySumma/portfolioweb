2015ba42dcf8aebeb99b3e9bcd06ed539dba0eda
CoreySumma <CoreySumma@gmail.com>
2023-02-26 20:09:48 -0600

    tweaks

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,57 +1,85 @@
-import React from 'react';
-import IMG1 from '../../assets/spaceman.png';
-import IMG2 from '../../assets/codemonk.png';
-import IMG3 from '../../assets/mistate.png';
-import IMG4 from '../../assets/spotlabs2.png';
+import React from "react";
+import IMG1 from "../../assets/spaceman.png";
+import IMG2 from "../../assets/codemonk.png";
+import IMG3 from "../../assets/mistate.png";
+import IMG4 from "../../assets/spotlabs2.png";
+import IMG5 from "../../assets/game_shelf.png";
+import IMG6 from "../../assets/SMSss.png";
 
-import './portfolio.css';
+import "./portfolio.css";
 
 export default function Portfolio() {
   const soloProjects = [
     {
       id: 1,
-      title: 'SpaceMan',
+      title: "SpaceMan",
       img: IMG1,
       description:
-        'A word guessing game that generates a random word and allows the user 5 attempts at guessing correct letters within it.  Do you have what it takes to save the Spaceman?',
-      technologies: 'Vanilla Javascript',
-      link: 'https://coreysumma.github.io/Spaceman/',
-      github: 'https://github.com/CoreySumma/Spaceman',
+        "A word guessing game that generates a random word and allows the user 5 attempts at guessing correct letters within it.  Do you have what it takes to save the Spaceman?",
+      technologies: "Vanilla Javascript",
+      link: "https://coreysumma.github.io/Spaceman/",
+      github: "https://github.com/CoreySumma/Spaceman",
     },
     {
       id: 2,
-      title: 'SpotLabs',
+      title: "SpotLabs",
       img: IMG4,
       description:
-        'A collaborate playlist application that allows a user to add/edit/delete songs on playlists that they create with their friends and family by embedding spotify directly onto the app.',
-      technologies: 'HTML | CSS |Javascript | Node | Express | MongoDB | Mongoose | Google Oauth',
-      link: 'https://mongoose-playlist-926.herokuapp.com/',
-      github: 'https://github.com/CoreySumma/mongoose-playlist',
+        "A collaborate playlist application that allows a user to add/edit/delete songs on playlists that they create with their friends and family by embedding spotify directly onto the app.",
+      technologies:
+        "HTML | CSS |Javascript | Node | Express | MongoDB | Mongoose | Google Oauth",
+      link: "https://mongoose-playlist-926.herokuapp.com/",
+      github: "https://github.com/CoreySumma/mongoose-playlist",
     },
     {
       id: 3,
-      title: '[codemonk]',
+      title: "[codemonk]",
       img: IMG2,
-      description: 'A full CRUD, Django driven collaboration for students to challenge and learn from one another by posting coding challenges, commenting on posts, and liking comments.',
-      technologies: ' | HTML | CSS | Python | Django | Javascript',
-      link: 'http://codemonk-wars.herokuapp.com/',
-      github: 'https://github.com/ultimoakim/-codemonk-',
+      description:
+        "A full CRUD, Django driven collaboration for students to challenge and learn from one another by posting coding challenges, commenting on posts, and liking comments.",
+      technologies:
+        " HTML | CSS | Python | Django | AWS | PostgresQL | Materialize",
+      link: "http://codemonk-wars.herokuapp.com/",
+      github: "https://github.com/ultimoakim/-codemonk-",
     },
     {
       id: 4,
-      title: 'MiState',
+      title: "MiState",
       img: IMG3,
       description:
-        'A MERN stack mindfulness app designed to track a users emotional state with daily entries that are dynamically organized.',
-      technologies: 'HTML | CSS | JavaScript | React | Node | Express | MongoDB | Bootstrap',
-      link: 'https://mistate.herokuapp.com/',
-      github: 'https://github.com/CoreySumma/mern-mistate',
+        "A MERN stack mindfulness app designed to track a users emotional state with daily entries that are dynamically organized. Login with un: guest pw: visitor123! to take a tour of the website.",
+      technologies:
+        "HTML | CSS | JavaScript | React | Node | Express | MongoDB | Bootstrap",
+      link: "https://mistate.herokuapp.com/",
+      github: "https://github.com/CoreySumma/mern-mistate",
+    },
+    {
+      id: 5,
+      title: "Game_Shelf",
+      img: IMG5,
+      description:
+        "An app designed to help gamers keep track of their progress for each videogame they own. Login with un: guest pw: visitor123! to take a tour of the website.",
+      technologies:
+        "HTML | CSS | Python | Django | AWS | PostgresQL | Materialize",
+      link: "https://videogameshelf.herokuapp.com/",
+      github: "https://github.com/CoreySumma/videogamecollector",
+    },
+    {
+      id: 6,
+      title: "AI SMS Generator",
+      img: IMG6,
+      description:
+        "A React application that utilizes Open AI's API, and Trilio's API to generate a random text to someone who is calling you so you can have some fun while filtering calls.",
+      technologies:
+        "HTML | CSS | React | Express | Twilio API | Open AI API",
+      link: "https://videogameshelf.herokuapp.com/",
+      github: "https://github.com/CoreySumma/videogamecollector",
     },
   ];
 
   return (
     <section id="portfolio">
-      <h5>My Recent Work</h5>
+      <h5>My Projects</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
         {soloProjects.map((pro) => (
@@ -84,7 +112,7 @@ export default function Portfolio() {
             </div>
           </article>
         ))}
-        </div>
+      </div>
     </section>
   );
 }
